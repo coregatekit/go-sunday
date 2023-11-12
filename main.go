ccaae686df6e73e12f6d0dc305de255644cdafa9
Joe Kim <joesm.genie@gmail.com>
2023-11-12 08:11:05 +0000

    Go Mod Init

--- main.go
+++ main.go
@@ -2,35 +2,10 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"myapp/libs"
 )
 
-func Ping1S(ch chan int) {
-	for i := 0; i < 10; i++ {
-		fmt.Println("ping : ", i)
-		time.Sleep(1 * time.Second)
-	}
-	ch <- 10
-}
-
-func SendNoti5s(ch chan string) {
-	fmt.Println("send noti...")
-	time.Sleep(5 * time.Second)
-	fmt.Println("Send noti...")
-	ch <- "success"
-}
-
 func main() {
-	fmt.Println("Go Routine & Channel")
-
-	chPing := make(chan int)
-	chNoti := make(chan string)
-
-	go Ping1S(chPing)
-	go SendNoti5s(chNoti)
-
-	// time.Sleep(12 * time.Second)
-	pingVal, notiMsg := <-chPing, <-chNoti
-	fmt.Println(pingVal, notiMsg)
-	fmt.Println("completed")
+	fmt.Println("Mod init")
+	libs.Greeting()
 }
