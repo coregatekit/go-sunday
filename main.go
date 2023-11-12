84bfa8494e82ab50b522163e47c4e9b1b602c5d3
Joe Kim <joesm.genie@gmail.com>
2023-11-12 03:46:06 +0000

    variables 1

--- main.go
+++ main.go
@@ -1,7 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+	"strconv"
+)
 
 func main() {
-	fmt.Println("hahaha")
+	// fmt.Println("enter your name: ")
+	// var txtInput string
+	// txtInput := ""
+	// fmt.Scan(&txtInput)
+	// fmt.Println("" + txtInput)
+
+	age := 2
+	fmt.Println(reflect.TypeOf((age)))
+
+	height := "100"
+
+	// var h int
+	// var err error
+	h, err := strconv.Atoi(height)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(h)
+
+	water := "12.40"
+	fWater, err := strconv.ParseFloat(water, 64)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(fWater)
+
+	txtShow := fmt.Sprintf("water = %8.2f, height = %d\n", fWater, h)
+	fmt.Print(txtShow)
 }
